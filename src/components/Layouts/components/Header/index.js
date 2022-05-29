7ace242f40f4babe9e227c2a8fdf9151f8f86042
Your Name <you@example.com>
2022-05-29 12:22:42 +0700

    64. Dựng khung Layout mặc định

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,13 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
 function Header() {
-    return <h1>Header layout</h1>;
+    return (
+        <header className={cx('wapper')}>
+            <div className={cx('inner')}></div>
+        </header>
+    );
 }
 
 export default Header;
